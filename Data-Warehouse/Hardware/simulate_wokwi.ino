6174c9dd7d92dbd19524b77ff4d6b8e3e92ab453
Pheak02 <saingsopheak02@gmail.com>
2025-01-26 12:05:52 +0700

    simulation device connect to thingsboard

--- Data-Warehouse/Hardware/simulate_wokwi.ino
+++ Data-Warehouse/Hardware/simulate_wokwi.ino
@@ -1,5 +1,7 @@
 #include <DHTesp.h>
 #include <WiFi.h>
+#include <ThingsBoard.h>
+#include <Arduino_MQTT_Client.h>
 
 #define pinDht 15
 DHTesp dhtSensor;
@@ -7,32 +9,65 @@ DHTesp dhtSensor;
 #define WIFI_AP "Wokwi-GUEST"
 #define WIFI_PASS ""
 
-void connectToWifi() {
+#define TB__SERVER "demo.thingsboard.io"
+#define TOKEN "m539cza3sEgnRj12Kxqr"
+
+constexpr uint16_t MAX_MESSAGE_SIZE = 128U;
+
+WiFiClient espClient;
+Arduino_MQTT_Client mqttClient(espClient);
+ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);
+
+void connectToWifi()
+{
   Serial.println("Connecting to WIFI...");
   int attempts = 0;
 
-  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+  while (WiFi.status() != WL_CONNECTED && attempts < 20)
+  {
     WiFi.begin(WIFI_AP, WIFI_PASS, 6);
     delay(500);
     Serial.print(".");
     attempts++;
-
   }
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("\nFailed to connect to WiFi.");
-  } else {
+  }
+  else
+  {
     Serial.println("\nConnected to WiFi");
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
-  dhtSensor.setup(pinDht,DHTesp::DHT22);
-  connectToWifi();
+void connectToThingsBoard()
+{
+  if (!tb.connected())
+  {
+    Serial.println("Connect to thingsboard server");
+
+    if (!tb.connect(TB__SERVER, TOKEN))
+    {
+      Serial.println("Failed to connect to Thingsboard");
+    }
+    else
+    {
+      Serial.println("Connected to Thingsboard");
+    }
+  }
 }
 
-void loop() {
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+  dhtSensor.setup(pinDht, DHTesp::DHT22);
+  connectToWifi();
+  connectToThingsBoard();
+}
+
+void loop()
+{
   connectToWifi();
 
   TempAndHumidity data = dhtSensor.getTempAndHumidity();
@@ -41,5 +76,10 @@ void loop() {
 
   Serial.println(temp);
   Serial.println(hum);
-  delay(10); //speeds up simulation
+  if (!tb.connected())
+  {
+    connectToThingsBoard();
+  }
+  delay(10); // speeds up simulation
+  tb.loop();
 }
